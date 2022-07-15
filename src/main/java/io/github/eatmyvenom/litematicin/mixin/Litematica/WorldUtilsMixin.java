b53d6e9bbbf43bfabce89d3acf8597270fb1749c
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2022-07-15 20:48:47 +0900

    Compat with masagadget

--- src/main/java/io/github/eatmyvenom/litematicin/mixin/Litematica/WorldUtilsMixin.java
+++ src/main/java/io/github/eatmyvenom/litematicin/mixin/Litematica/WorldUtilsMixin.java
@@ -1,5 +1,6 @@
 package io.github.eatmyvenom.litematicin.mixin.Litematica;
 
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -7,16 +8,24 @@ import fi.dy.masa.litematica.util.WorldUtils;
 import io.github.eatmyvenom.litematicin.utils.Printer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = WorldUtils.class, remap = false, priority = 1010)
 public class WorldUtilsMixin {
+	private static boolean hasSent = false;
     /**
      * @author joe mama // fixed by AngelBottomless
      */
     //@Overwrite
-    @Overwrite()
-    private static ActionResult doEasyPlaceAction(MinecraftClient mc)
+    @Inject(
+	    method = "doEasyPlaceAction",
+	    at = @At("HEAD"),
+	    cancellable = true
+    )
+    private static void onDoEasyPlaceAction(MinecraftClient mc, CallbackInfoReturnable<ActionResult> cir)
     {
 		ActionResult defaultResult =  ActionResult.SUCCESS;
 		try {
@@ -25,7 +34,12 @@ public class WorldUtilsMixin {
 		catch (NullPointerException e){
 			//in case of NPE, print log instead
 			e.printStackTrace();
+			if (!hasSent && mc.player != null){
+				mc.player.sendMessage(Text.of("Null pointer exception has occured, please upload log at https://github.com/aria1th/litematica-printer/issues"));
+				hasSent = true;
+			}
 		}
-		return defaultResult;
+		cir.setReturnValue(defaultResult);
+		//return defaultResult;
     }
 }
